7c668126d16824f43ad637b1b221ffc8d701f286
Abhra Biswas <abhra.develop@gmail.com>
2020-10-19 17:13:03 -0500

    improvement: Adds App.jsx
    Parent component is comprised of the 5 sections that make up the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-class App extends React.Component {
-  render() {
-    return <h1>Hello, world.</h1>;
-  }
-}
+import Social from "./organisms/Social";
+import Header from "./organisms/Header";
+import Hero from "./organisms/Hero";
+import CTABanner from "./organisms/CTABanner";
+import Content from "./organisms/Content";
+
+const App = () => {
+  return (
+    <>
+      <Social />
+      <Header />
+      <Hero />
+      <CTABanner />
+      <Content />
+    </>
+  );
+};
 
 export default App;
