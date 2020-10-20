4616836c93a2a43f976e87bbc2bdb12c1403dd9b
Abhra Biswas <abhra.develop@gmail.com>
2020-10-19 22:38:09 -0500

    improvement: CTA Banner styled

--- src/organisms/CTABanner.js
+++ src/organisms/CTABanner.js
@@ -9,22 +9,40 @@ const Flexbox = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: center;
 
   background-color: #e24301;
   box-shadow: 0px 2px 5px grey;
   height: 120px;
+`;
 
+const Wrapper = {
+  padding: "20px",
+};
+
+const Text = styled.h4`
   font-family: Montserrat;
+  font-size: 17px;
+  font-weight: 175;
   color: white;
 `;
 
 const CTABanner = () => {
   return (
     <Flexbox>
-      <h4 style={TextStyle}>
-        We make the web a better place. View our work to find out how.
-      </h4>
-      <StyledButton padding={100} filled={false} text="Our Projects" url="#" />
+      <div style={Wrapper}>
+        <Text>
+          We make the web a better place. View our work to find out how.
+        </Text>
+      </div>
+      <div style={Wrapper}>
+        <StyledButton
+          padding={100}
+          filled={false}
+          text="Our Projects"
+          url="#"
+        />
+      </div>
     </Flexbox>
   );
 };
