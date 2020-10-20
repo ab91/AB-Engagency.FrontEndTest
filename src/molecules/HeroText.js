4eda1b616f39ec8f1e60366c4cea0249b7305420
Abhra Biswas <abhra.develop@gmail.com>
2020-10-19 20:49:21 -0500

    improvement: Hero section styled
    Button CSS outstanding

--- src/molecules/HeroText.js
+++ src/molecules/HeroText.js
@@ -6,26 +6,52 @@ import breakpoint from "styled-components-breakpoint";
 import StyledButton from "../atoms/buttons/StyledButton";
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  justify-content: center;
   font-family: Montserrat;
-  color: black;
+  color: white;
+
+  margin-left: 18%;
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  margin: 0;
+  font-size: 40px;
+  font-weight: 375;
+  letter-spacing: 2px;
+`;
 
-const Message = styled.h2``;
+const Message = styled.h2`
+  margin: 16px 0px;
+  font-size: 18px;
+  font-weight: 350;
+  line-height: 1.5;
+`;
+
+const ButtonFlexbox = {
+  display: "flex",
+  flexDirection: "row",
+};
 
 const HeroText = (props) => {
   return (
     <Wrapper>
-      <Title>Welcome to ENGAGEncy</Title>
+      <Title>
+        WELCOME TO <strong>ENGAGE</strong>NCY
+      </Title>
       <Message>
         We are a web developement company based in Austin, TX specializing in
         the Sitecore
         <br />
         CMS platform. we work hard, we play hard, and we eat lunch together.
       </Message>
-      <StyledButton text="Learn More" />
-      <StyledButton text="Get in Touch" />
+      <div style={ButtonFlexbox}>
+        <StyledButton text="Learn More" />
+        <StyledButton text="Get in Touch" />
+      </div>
     </Wrapper>
   );
 };
--- src/organisms/Hero.js
+++ src/organisms/Hero.js
@@ -5,8 +5,23 @@ import breakpoint from "styled-components-breakpoint";
 
 import HeroText from "../molecules/HeroText";
 
+const BackgroundDiv = styled.div`
+  background-image: url("https://res.cloudinary.com/ab91/image/upload/v1603157318/Engagency%20Project/skyline_hero.jpg");
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  background-repeat: no-repeat;
+
+  height: 500px;
+  width: 100%;
+`;
+
 const Hero = () => {
-  return <HeroText />;
+  return (
+    <BackgroundDiv>
+      <HeroText />
+    </BackgroundDiv>
+  );
 };
 
 export default Hero;
