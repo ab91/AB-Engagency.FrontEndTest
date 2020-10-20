58b19e34b58f5b5f178e6db5d092e08a75e9fb16
Abhra Biswas <abhra.develop@gmail.com>
2020-10-20 00:23:51 -0500

    improvement: Adds ThemeProvider styled-components wrapper, setting custom breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 
+import {ThemeProvider} from 'styled-components';
+
 import Social from "./organisms/Social";
 import Header from "./organisms/Header";
 import Hero from "./organisms/Hero";
 import CTABanner from "./organisms/CTABanner";
 import Content from "./organisms/Content";
 
+const theme = {
+  breakpoints: { 
+    mobile: 0,
+    tablet: 768,
+    desktop: 1025,
+  }
+};
+
 const App = () => {
   return (
-    <>
+      <ThemeProvider theme={theme}>
       <Social />
       <Header />
       <Hero />
       <CTABanner />
       <Content />
-    </>
+    </ThemeProvider>
   );
 };
 
