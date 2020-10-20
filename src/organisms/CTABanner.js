bab554ef6f88171ba33f7453716492118486c237
Abhra Biswas <abhra.develop@gmail.com>
2020-10-20 00:29:41 -0500

    improvement: Makes CTA Banner responsive

--- src/organisms/CTABanner.js
+++ src/organisms/CTABanner.js
@@ -7,42 +7,96 @@ import StyledButton from "../atoms/buttons/StyledButton";
 
 const Flexbox = styled.div`
   display: flex;
-  flex-direction: row;
   align-items: center;
-  justify-content: center;
 
   background-color: #e24301;
   box-shadow: 0px 2px 5px grey;
+
+  ${breakpoint("mobile")`
+  flex-direction: column;
+  justify-content: center;
+  height: 210px;
+    `}
+
+  ${breakpoint("tablet")`
+  flex-direction: row;
+  height: 175px;
+    `}
+
+  ${breakpoint("desktop")`
+  flex-direction: row;
   height: 120px;
+  justify-content: center;
+    `}
 `;
 
-const Wrapper = {
-  padding: "20px",
-};
+const Wrapper = styled.div`
+  ${breakpoint("mobile")`
+  padding: 10px;
+  margin: 5px;
+    `}
+
+  ${breakpoint("tablet")`
+  margin: 10px;
+    `}
+
+  ${breakpoint("desktop")`
+  padding: 20px;
+    `}
+`;
 
 const Text = styled.h4`
   font-family: Montserrat;
   font-size: 17px;
   font-weight: 175;
   color: white;
+  line-height: 1.7;
+
+  ${breakpoint("mobile")`
+  text-align: center;
+  font-size: 20px;
+  font-weight: 200;
+  margin: 0;
+    `}
+
+  ${breakpoint("tablet")`
+  text-align: left;
+    `}
+
+  ${breakpoint("desktop")`
+  text-align: left;
+  font-size: 17px;
+  font-weight: 175;
+    `}
+`;
+
+const Break = styled.br`
+  ${breakpoint("mobile")`
+  display: static;
+    `}
+
+  ${breakpoint("desktop")`
+  display: none;
+    `}
 `;
 
 const CTABanner = () => {
   return (
     <Flexbox>
-      <div style={Wrapper}>
+      <Wrapper>
         <Text>
-          We make the web a better place. View our work to find out how.
+          We make the web a better place.
+          <Break /> View our work to find out how.
         </Text>
-      </div>
-      <div style={Wrapper}>
+      </Wrapper>
+      <Wrapper>
         <StyledButton
           padding={100}
           filled={false}
           text="Our Projects"
           url="#"
         />
-      </div>
+      </Wrapper>
     </Flexbox>
   );
 };
