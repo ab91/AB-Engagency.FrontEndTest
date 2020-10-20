c7455ada72dc63afd11fc854d6832747291120a7
Abhra Biswas <abhra.develop@gmail.com>
2020-10-19 20:03:01 -0500

    improvement: Main body of webpage styled
    Feature components placed in Flexbox, Flexboxes placed in Parent Grid in Content component. Copy updated for Maintenance feature.

--- src/molecules/Feature.js
+++ src/molecules/Feature.js
@@ -3,6 +3,17 @@ import React from "react";
 import styled from "styled-components";
 import breakpoint from "styled-components-breakpoint";
 
+const Flexbox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  font-family: Montserrat;
+  color: #333333;
+  text-align: center;
+  line-height: 1.5;
+`;
+
 const Image = styled.img({
   height: "100px",
   width: "100px",
@@ -12,17 +23,23 @@ const Image = styled.img({
   //margin: "2% auto",
 });
 
-const Header = styled.p``;
+const Header = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+`;
 
-const Description = styled.p``;
+const Description = styled.p`
+  font-size: 15px;
+  font-weight: 300;
+`;
 
 const Feature = (props) => {
   return (
-    <>
+    <Flexbox>
       <Image src={props.image} />
       <Header>{props.header}</Header>
       <Description>{props.description}</Description>
-    </>
+    </Flexbox>
   );
 };
 
--- src/organisms/Content.js
+++ src/organisms/Content.js
@@ -5,6 +5,13 @@ import breakpoint from "styled-components-breakpoint";
 
 import Feature from "../molecules/Feature";
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 30%);
+  justify-content: space-around;
+  margin: 5% 10%;
+`;
+
 const ArchitectureProps = {
   image:
     "https://res.cloudinary.com/ab91/image/upload/v1603149096/Engagency%20Project/Architecture.png",
@@ -18,7 +25,7 @@ const MaintenanceProps = {
     "https://res.cloudinary.com/ab91/image/upload/v1603149063/Engagency%20Project/Maintenance.png",
   header: "Stress Free Maintenance",
   description:
-    "Our job is to make you look good, and help you deliver award-winning results. We're as committed as you are to ensuring that your Sitecore projects look and work as your designers intended, and your clients’ are getting the most out of Sitecore.",
+    "Our team of experts are excited to help you execute your monthly todo list, perform upgrades, manage your hosting environment, educate and train your team to get the most out of Sitecore, and are always ready to respond to an emergency.",
 };
 
 const AuditsProps = {
@@ -31,11 +38,11 @@ const AuditsProps = {
 
 const Content = () => {
   return (
-    <>
+    <Grid>
       <Feature {...ArchitectureProps} />
       <Feature {...MaintenanceProps} />
       <Feature {...AuditsProps} />
-    </>
+    </Grid>
   );
 };
 
