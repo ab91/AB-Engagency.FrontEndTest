f1503c5499d9e2808b4b819baf5b1366bc89e124
Abhra Biswas <abhra.develop@gmail.com>
2020-10-19 22:27:36 -0500

    improvement: Adds conditional styling to StyledButton component
    Styling based on props received by component

--- src/atoms/buttons/StyledButton.js
+++ src/atoms/buttons/StyledButton.js
@@ -3,14 +3,46 @@ import React from "react";
 import styled from "styled-components";
 import breakpoint from "styled-components-breakpoint";
 
-const Button = styled.button`
-  background-color: white;
-  height: 35px;
-  width: 120px;
-`;
+//const Button = styled.button`
+//border: 1px solid #FFFFFF;
+//border-radius: 5px;
+//background: white;
+
+//color: #FFFFFF;
+//font-size: 16px;
+//cursor: pointer;
+//`;
+
+const Button = styled.a((props) => ({
+  border: "1px solid #FFFFFF",
+  borderRadius: "5px",
+  backgroundColor: props.filled ? "#FFFFFF" : "transparent",
+  color: props.filled ? "#333333" : "#FFFFFF",
+
+  paddingTop: "12px",
+  paddingBottom: "12px",
+  paddingLeft: props.padding,
+  paddingRight: props.padding,
+
+  cursor: "pointer",
+  textDecoration: "none",
+
+  fontFamily: "Montserrat",
+  fontSize: "16px",
+  fontWeight: "350",
+}));
 
 const StyledButton = (props) => {
-  return <Button>{props.text}</Button>;
+  return (
+    <Button
+      padding={props.padding}
+      filled={props.filled}
+      href={props.url}
+      target="_blank"
+    >
+      {props.text}
+    </Button>
+  );
 };
 
 export default StyledButton;
