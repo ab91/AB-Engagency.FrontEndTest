432534c7b7083dbea855b0738ec2efaa15864161
Abhra Biswas <abhra.develop@gmail.com>
2020-10-19 23:08:00 -0500

    improvement: Social component styled
    SVG as link outstanding

--- src/organisms/Social.js
+++ src/organisms/Social.js
@@ -11,21 +11,41 @@ const Flexbox = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: space-between;
+  padding: 0px 55px;
 
   background-color: #333333;
-  height: 28px;
+  height: 50px;
 
   font-family: Montserrat;
+  font-weight: 300;
+  letter-spacing: 1.1px;
   color: white;
 `;
+
+const FlexContainer = {
+  display: "flex",
+};
+
+const SVGButtonStyle = {
+  padding: "0px 5px",
+};
 
 const Social = () => {
   return (
     <Flexbox>
       <p>Call us at: (888) 303-7332</p>
-      <SvgFacebookIcon />
-      <SvgLinkedinIcon />
-      <SvgTwitterIcon />
+      <div style={FlexContainer}>
+        <div style={SVGButtonStyle}>
+          <SvgFacebookIcon />
+        </div>
+        <div style={SVGButtonStyle}>
+          <SvgLinkedinIcon />
+        </div>
+        <div style={SVGButtonStyle}>
+          <SvgTwitterIcon />
+        </div>
+      </div>
     </Flexbox>
   );
 };
